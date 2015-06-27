fb60454e57a8bfcac030d31e2a630054d9fbf95a
Jonathan Gertig <jcgertig@gmail.com>
2015-06-27 15:04:38 -0400

    custom success

--- app/assets/javascripts/activeadmin-sortable.js
+++ app/assets/javascripts/activeadmin-sortable.js
@@ -6,13 +6,20 @@
   $.fn.activeAdminSortable = function() {
     this.sortable({
       update: function(event, ui) {
-        var url = ui.item.find('[data-sort-url]').data('sort-url');
+        var item = ui.item.find('[data-sort-url]');
+        var url = item.data('sort-url');
+        var actionOnSuccess = item.data('sort-success-action');
+        var customParams = {};
+        if (typeof item.data('sort-custom-params') === 'object') {
+          customParams = item.data('sort-custom-params');
+        }
 
         $.ajax({
           url: url,
           type: 'post',
-          data: { position: ui.item.index() + 1 },
-          success: function() { window.location.reload() }
+          data: $.extend(customParams, { position: ui.item.index() + 1 }),
+          error: function() { console.error("Saving sortable error"); },
+          success: function() { if (actionOnSuccess='noting') { return; } else { window.location.reload(); }}
         });
       }
     });
