38b6c4e1ec9b1ad874675760eaf1eb5afbe043d4
Nikita Sokolov <faucct@gmail.com>
2018-07-20 13:37:09 +0300

    Rails 5 should be supported

--- Gemfile
+++ Gemfile
@@ -2,7 +2,10 @@ source 'https://rubygems.org'
 
 gemspec
 
-gem 'activeadmin', github: 'activeadmin/activeadmin'
+gem 'activeadmin'
+gem 'coffee-script'
+gem 'puma'
+gem 'rails', *ENV['RAILS_VERSION']
 gem 'sqlite3'
 gem 'acts_as_list'
-gem 'rubocop'
+gem 'rubocop', '0.34.2'
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -21,7 +21,9 @@ module Dummy
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    if Rails.gem_version < Gem::Version.new('5')
+      config.active_record.raise_in_transactional_callbacks = true
+    end
   end
 end
 
